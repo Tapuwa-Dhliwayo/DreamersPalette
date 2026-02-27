80e64b344ab661bc29a0ae729d3cb664a7a32998
Tapuwa Dhliwayo <tapsdhliwayo@gmail.com>
2026-02-27 22:27:11 +0200

    Issue with system dark mode constraints on the logo.

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -12,8 +12,8 @@ export default function Logo({
     }
 
     const colorVariants = {
-        default: "text-neutral-900! dark:text-neutral-100!",
-        subtle: "opacity-70 text-neutral-900! dark:text-neutral-100!"
+        default: "text-neutral-700!",
+        subtle: "opacity-70 text-neutral-700!"
     }
 
     const accentStyle =
