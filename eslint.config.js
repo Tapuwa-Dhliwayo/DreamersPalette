bd4890adba44f2b942f139a5c324b6d032d88a1e
Tapuwa Dhliwayo <tapsdhliwayo@gmail.com>
2026-08-04 13:09:02 +0200

    Ignore leftover dist-root-owned-old artifact in ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', '.agents', '.codex', '.impeccable']),
+  globalIgnores(['dist', 'dist-root-owned-old', '.agents', '.codex', '.impeccable']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
